4c335b217e21cf072434618c7cb873a91b2fdfd9
gojko1001 <gojko.novcic2@gmail.com>
2020-01-10 02:34:52 +0100

    #status_bar Popravljen sat

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -1,3 +1,5 @@
+package main;
+
 import controller.PredmetController;
 import model.PredmetBaza;
 import model.StudentBaza;
--- src/view/StatusBar.java
+++ src/view/StatusBar.java
@@ -1,6 +1,8 @@
 package view;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,6 +10,7 @@ import java.util.Date;
 import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class StatusBar extends JMenuBar{
 	
@@ -15,10 +18,19 @@ public class StatusBar extends JMenuBar{
 	
 	public StatusBar() {
 		JLabel name = new JLabel("Studentska služba");
-		DateFormat df = new SimpleDateFormat("HH:mm      dd-MM-yyyy.   ");
-		Date date = new Date();
-		JLabel time = new JLabel(df.format(date));
+		JLabel time = new JLabel();
 		
+		Timer timer = new Timer(1000, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Date date = new Date();
+				DateFormat df = new SimpleDateFormat("HH:mm:ss      dd.MM.yyyy.   ");
+				time.setText(df.format(date));
+			}
+		});
+		
+		timer.start();
 		JPanel leftSide = new JPanel();
 		leftSide.setLayout(new FlowLayout(FlowLayout.LEFT));
 		leftSide.add(name);
